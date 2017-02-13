a695eac9d59e6e92eb725aa57425f718c92a932a
nir <nir@stratoscale.com>
2017-02-13 17:42:56 +0200

    change install files location
    from the elusive tmp dir to /home/fedora/install

--- ansible/roles/kubernetes/files/make-ca-cert.sh
+++ ansible/roles/kubernetes/files/make-ca-cert.sh
@@ -96,7 +96,7 @@ service_ip=$(echo "${octets[*]}" | sed 's/ /./g')
 # Determine appropriete subject alt names
 sans="IP:${cert_ip},IP:${service_ip},DNS:kubernetes,DNS:kubernetes.default,DNS:kubernetes.default.svc,DNS:kubernetes.default.svc.${dns_domain},DNS:${master_name}"
 
-cd /tmp/easy-rsa-master/easyrsa3
+cd /home/fedora/install/easy-rsa-master/easyrsa3
 
 # Sadly, openssl is very verbose to std*err* with no option to turn it off.
 if ! (./easyrsa --batch init-pki
